ae599b9f2a9c6c79d5d79aa6a4a7ed7f5381dd56
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-08-10 18:05:13 +0200

    Added files

--- LoginRxSwift/ViewController.swift
+++ LoginRxSwift/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func loginTapped(_ sender: Any) {
+        
+    }
 
 }
 
